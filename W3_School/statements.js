17d95e02ba86878362df808d25e8849a0a63abbe
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-10-01 13:26:54 +0600

    Add files via upload

---
+++ W3_School/statements.js
@@ -0,0 +1,14 @@
+let x, y, z;
+
+x = 5;
+y = 6;
+z = x + y;
+
+console.log(z);
+
+let person1, person2, person3;
+person1 = "pallab ";
+person2 = "Majumdar";
+
+person3 = person1 + person2;
+console.log(person3);
