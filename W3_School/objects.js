e43a81b97a00e4e3272b99641df7ab6594c44e60
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-10-01 19:03:35 +0600

    Add files via upload

---
+++ W3_School/objects.js
@@ -0,0 +1,10 @@
+person = {
+  first_name: "pallab",
+  last_name: "majumdar",
+  age: 23,
+  location: "Jashore,Rupdia",
+};
+
+console.log(person.first_name);
+console.log(person.last_name);
+console.log(person.age);
