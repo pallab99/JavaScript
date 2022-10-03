98cebd6dbd5f95677d8bfc5c63d806bc5e7d9ff4
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-10-03 22:26:29 +0600

    Add files via upload

---
+++ W3_School/arrays_method.js
@@ -0,0 +1,30 @@
+let cars=["tata","toyota","feat"];
+console.log(cars);
+
+cars.push("Audi");
+
+console.log(cars);
+
+cars.pop();
+
+console.log(cars);
+
+let shft=cars.shift();
+
+console.log(shft);
+
+console.log(cars);
+
+let ushft=cars.unshift("tata");
+
+console.log(ushft);
+
+console.log(cars);
+
+let myGirls = ["Cecilie", "Lone"];
+let myBoys = ["Emil", "Tobias", "Linus"];
+
+let myChildren = myGirls.concat(myBoys);
+
+console.log(myChildren);
+
