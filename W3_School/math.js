8a95764da23e9080628caae720d5676cb3dac548
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-10-21 20:43:13 +0600

    Add files via upload

---
+++ W3_School/math.js
@@ -0,0 +1,58 @@
+Math.round(4.6);
+console.log(Math.round(4.6));
+console.log(Math.round(4.2));
+
+// ! Math.ceil(x) returns the value of x rounded up to its nearest integer:
+console.log(Math.ceil(4.1));
+
+// ! Math.floor(x) returns the value of x rounded down to its nearest integer:
+console.log(Math.floor(4.8));
+
+// ! Math.trunc(x) returns the integer part of x:
+console.log(Math.trunc(4.5));
+
+// ! Math.sign(x) returns if x is negative, null or positive:
+console.log(Math.sign(-10));
+
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(16));
+console.log(Math.abs(-4));
+
+console.log(Math.min(0, 150, 30, 20, -8, -200));
+
+console.log(Math.max(0, 150, 30, 20, -8, -200));
+
+// ! Math.log(x) returns the natural logarithm of x.
+
+console.log(Math.log(4));
+console.log(Math.log2(4));
+console.log(Math.log10(4));
+
+// ! Method	Description
+// ! abs(x)	Returns the absolute value of x
+// ! acos(x)	Returns the arccosine of x, in radians
+// ! acosh(x)	Returns the hyperbolic arccosine of x
+// ! asin(x)	Returns the arcsine of x, in radians
+// ! asinh(x)	Returns the hyperbolic arcsine of x
+// ! atan(x)	Returns the arctangent of x as a numeric value between -PI/2 and PI/2 radians
+// ! atan2(y, x)	Returns the arctangent of the quotient of its arguments
+// ! atanh(x)	Returns the hyperbolic arctangent of x
+// ! cbrt(x)	Returns the cubic root of x
+// ! ceil(x)	Returns x, rounded upwards to the nearest integer
+// ! cos(x)	Returns the cosine of x (x is in radians)
+// ! cosh(x)	Returns the hyperbolic cosine of x
+// ! exp(x)	Returns the value of Ex
+// ! floor(x)	Returns x, rounded downwards to the nearest integer
+// ! log(x)	Returns the natural logarithm (base E) of x
+// ! max(x, y, z, ..., n)	Returns the number with the highest value
+// ! min(x, y, z, ..., n)	Returns the number with the lowest value
+// ! pow(x, y)	Returns the value of x to the power of y
+// ! random()	Returns a random number between 0 and 1
+// ! round(x)	Rounds x to the nearest integer
+// ! sign(x)	Returns if x is negative, null or positive (-1, 0, 1)
+// ! sin(x)	Returns the sine of x (x is in radians)
+// ! sinh(x)	Returns the hyperbolic sine of x
+// ! sqrt(x)	Returns the square root of x
+// ! tan(x)	Returns the tangent of an angle
+// ! tanh(x)	Returns the hyperbolic tangent of a number
+// ! trunc(x)	Returns the integer part of a number (x)
